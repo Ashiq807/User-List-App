62e04be4fe9ad92ff8dc93717ded0daaf0c3c545
Ashiq807 <ashiqos807@gmail.com>
2024-08-15 21:15:56 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import AddUser from './components/Users/AddUser';
+import UsersLIst from './components/Users/UsersList';
 
 function App() {
   return (
     <div>
-
+      <AddUser />
+      <UsersLIst users={[]} />
     </div>
   );
 }
